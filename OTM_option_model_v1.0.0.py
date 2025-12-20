00de5f40792efdca7996d6884941b03b25e93a29
Steven Leung <stevenleung250@gmail.com>
2025-12-20 11:55:14 -0500

    Update OTM_option_model_v1.0.0.py

--- OTM_option_model_v1.0.0.py
+++ OTM_option_model_v1.0.0.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[38]:
-
-
 #libraries
 import yfinance as yf
 import pandas as pd
@@ -376,10 +370,3 @@ def option_spread():
     plt.show()
 
 option_spread()
-
-
-# In[ ]:
-
-
-
-
